d350a032c6f0249a4a172ef406362e54335d3d12
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-11-11 18:12:37 +0600

    javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+// let age=34;
+
+// if(age>=18){
+//     console.log("biyer boyos done");
+// }else{
+//     console.log("biyer boyos not done");
+// }
+
+let age=20;
+let biya=(age>=18)? "biyer boyos done" : "biyer boyos not done";
+console.log(biya);
+
+
+// nullish coalescing
+
+let name=null;
+console.log(name ?? "jihan" );
